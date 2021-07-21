5eca12a59bdb172607e103b415ca2073843bfa26
Hayden <haydencq@icloud.com>
2021-07-20 22:38:13 -0400

    final commit

--- src/pomodoro/Pomodoro.js
+++ src/pomodoro/Pomodoro.js
@@ -1,154 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import classNames from "../utils/class-names";
-import useInterval from "../utils/useInterval";
 import secondsToDuration, {minutesToDuration} from "../utils/duration";
+import Logic from "../pomodoro/PomodoroLogic"
 
-// These functions are defined outside of the component to insure they do not have access to state
-// and are, therefore more likely to be pure.
-
-/**
- * Update the session state with new state after each tick of the interval.
- * @param prevState
- *  the previous session state
- * @returns
- *  new session state with timing information updated.
- */
-function nextTick(prevState) {
-  const timeRemaining = Math.max(0, prevState.timeRemaining - 1);
-  return {
-    ...prevState,
-    timeRemaining,
-  };
-}
-
-/**
- * Higher order function that returns a function to update the session state with the next session type upon timeout.
- * @param focusDuration
- *    the current focus duration
- * @param breakDuration
- *    the current break duration
- * @returns
- *  function to update the session state.
- */
-function nextSession(focusDuration, breakDuration) {
-  /**
-   * State function to transition the current session type to the next session. e.g. On Break -> Focusing or Focusing -> On Break
-   */
-  return (currentSession) => {
-    if (currentSession.label === "Focusing") {
-      return {
-        label: "On Break",
-        timeRemaining: breakDuration * 60,
-      };
-    }
-    return {
-      label: "Focusing",
-      timeRemaining: focusDuration * 60,
-    };
-  };
-}
 
 function Pomodoro() {
+  // Destructure our logic function so we have access to the individual functions
+  const { decrease, increase, stop, getValueNow, getWidth, focusDuration, breakDuration, session, isTimerRunning, playPause } = Logic()
+
   // Timer starts out paused
-  const [isTimerRunning, setIsTimerRunning] = useState(false);
-
-  // has a LABEL and a timeRemaining value
-  const [session, setSession] = useState(null);
-
-
-  const [focusDuration, setFocusDuration] = useState(25);
-  const [breakDuration, setBreakDuration] = useState(5);
-
-  function decrease(event) {
-    const testId = event.target.dataset.testid
-    
-    if(!isTimerRunning) {
-      if (testId === 'decrease-break' && breakDuration - 1 >= 1) {
-        setBreakDuration(breakDuration - 1) 
-      } else if (testId === 'decrease-focus' && focusDuration - 5 >= 5) {
-        setFocusDuration(focusDuration - 5)
-      }
-    }
-  }
-
-  function increase(event) {
-    const testId = event.target.dataset.testid
-
-    if(!isTimerRunning) {
-      if (testId === 'increase-break' && breakDuration + 1 <= 15) {
-        setBreakDuration(breakDuration + 1) 
-      } else if (testId === 'increase-focus' && focusDuration + 5 <= 60) {
-        setFocusDuration(focusDuration + 5)
-      }
-    }
-  }
-
-  function stop() {
-    setSession(null)
-    setIsTimerRunning(false)
-  }
-
   
-
-  function getWidth() {
-    if (!session) return 0;
-
-    if (session.label === "Focusing") {
-      return `${100 - (session.timeRemaining / (focusDuration * 60) * 100)}%`
-    } else {
-      return `${100 - (session.timeRemaining / (breakDuration * 60) * 100)}%`
-    }
-  }
-  
-  function getValueNow() {
-    if (!session) return 0;
-
-    if (session.label === "Focusing") {
-      return (100 - (session.timeRemaining / (focusDuration * 60) * 100))
-    } else {
-      return (100 - (session.timeRemaining / (breakDuration * 60) * 100))
-    }
-  }
-  
-
-  /**
-   * Custom hook that invokes the callback function every second
-   *
-   * NOTE: You will not need to make changes to the callback function
-   */
-  useInterval(() => {
-      if (session.timeRemaining === 0) {
-        new Audio("https://bigsoundbank.com/UPLOAD/mp3/1482.mp3").play();
-        return setSession(nextSession(focusDuration, breakDuration));
-      }
-      return setSession(nextTick);
-    },
-    isTimerRunning ? 1000 : null
-  );
-
-  /**
-   * Called whenever the play/pause button is clicked.
-   */
-  function playPause() {
-    setIsTimerRunning((prevState) => {
-      const nextState = !prevState;
-      if (nextState) {
-        setSession((prevStateSession) => {
-          // If the timer is starting and the previous session is null,
-          // start a focusing session.
-          if (prevStateSession === null) {
-            return {
-              label: "Focusing",
-              timeRemaining: focusDuration * 60,
-            };
-          }
-          return prevStateSession;
-        });
-      }
-      return nextState;
-    });
-
-  }
 
 
   return (
